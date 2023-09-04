c8e1686aa3fdeaa62bcdb08bf358fffd54e3a4b3
Biplob Modak <119670070+biplobDlab@users.noreply.github.com>
2023-09-04 19:31:06 +0600

    AR glass RND Day 1
    * General Project setting.
    * Testing and Placement Management.
    * Glass Placement.

---
+++ Assets/Scripts/GlassSelectionManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassSelectionManager : MonoBehaviour
+{
+    [SerializeField] GameObject correntGlass;
+
+
+    public void SelectGlass() 
+    {
+
+    }
+    
+}
